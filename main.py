00e9603d5fe22ae8dcc05519b07ce6cc26e2075f
Дима <86681516+D1roX@users.noreply.github.com>
2023-08-29 03:31:55 +0300

    upd main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from logger import Logger
-import threading
 
 
 def logger_test():
     log = Logger()
-    logger_thread = threading.Thread(target=log.make_log, args=('bob',), daemon=True)
     log.loger(0, 'test SYSTEM')
     log.loger(3, 'test INFO and ERROR', True)
     log.loger(2, 'test DIAGNOST')
