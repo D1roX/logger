e5d337331781824bca542c338d58aab75d918a5d
Дима <86681516+D1roX@users.noreply.github.com>
2023-09-02 00:20:41 +0300

    Add files via upload

--- logger.py
+++ logger.py
@@ -1,57 +1,150 @@
 import logging
-import os
+from logging.handlers import RotatingFileHandler
+import sys
 import threading
-
 
 # уровни логирования
 LOGGING_LEVELS = {
-    0: 'SYSTEM',
-    1: 'INIT',
-    2: 'DIAGNOST',
-    3: 'INFO',
+    10: 'DEBUG',
+    20: 'INFO',
+    30: 'WARNING',
+    40: 'ERROR',
+    50: 'CRITICAL',
+    60: 'SYSTEM',
+    70: 'INIT',
+    80: 'DIAGNOST',
+}
+
+# допустимые уровни логирования для handler'ов
+HANDLER_LEVELS = {
+    'CONSOLE_LEVEL': 10,
+    'FILE_LEVEL': 60,
+    'CUSTOM_CONSOLE_LEVELS': [10, 20, 30, 40, 50, 60, 70, 80],
+    'CUSTOM_FILE_LEVELS': [20, 60, 70, 80],
 }
 
 # формат сообщения
-MESSAGE_FORMAT = '%(asctime)s - %(name)s - %(message)s'
+MESSAGE_FORMAT = '%(asctime)s | %(name)-20s | %(message)s'
+
+
+def make_msg(msg, is_error, level_index=10, level_name=None):
+    """
+    Функция добавления информации об уровне логирования и наличию ошибки к сообщению.
+    :param msg: сообщение
+    :param is_error: наличие ошибки
+    :param level_index: индекс уровня логирования
+    :param level_name: имя уровня логирования
+    :return: итоговое сообщение
+    """
+    error = '[ERROR]' if is_error else ''
+    if level_name is None:
+        level_name = f'[{LOGGING_LEVELS[level_index]}]'
+    msg = f'{error: <7} {level_name: <10} | {msg}'
+    return msg
+
+
+def isEnabledFor(level_index):
+    """
+    Функция для проверки наличия переданного уровня логирования в словаре существующих.
+    :param level_index: индекс уровня логирования.
+    :return:
+    True, если переданный индекс существует в словаре;
+    False, если отсутствует.
+    """
+    if level_index in LOGGING_LEVELS.keys():
+        return True
+    else:
+        return False
+
+
+def set_level_filter(levels=None):
+    """
+    Функция создания кастомного фильтра.
+    :param levels: допустимые уровни логирования
+    :return: фильтр
+    """
+    if levels is None:
+        levels = HANDLER_LEVELS['CUSTOM_CONSOLE_LEVELS']
+    return Filter(levels)
+
+
+class Filter(logging.Filter):
+    """
+    Класс кастомного фильтра для регистрации указанных уровней логирования и отсвеивания остальных.
+    """
+    def __init__(self, levels):
+        """
+        Инициализация допустимых уровней.
+        :param levels: список допустимых уровней
+        """
+        self.__levels = levels
+
+    def filter(self, log_record):
+        """
+        Функция, проверяющая наличие уровня из log_record в списке допустимых уровней __levels.
+        :param log_record: запись лога
+        :return:
+        True, если уровень существует в списке допустимых уровней;
+        False, если отсутствует.
+        """
+        if log_record.levelno in self.__levels:
+            return True
+        return False
 
 
 class Logger(logging.getLoggerClass()):
     """
     Класс логирования.
     """
-    def __init__(self, name='logger'):
+
+    def __init__(self, name='logger', console_level=None, file_level=None):
+        """
+        Инициализация логгера.
+        :param name: имя логгера
+        """
         super().__init__(name)
 
-        # изменение имени файла логирования предыдущего запуска, если он существует
-        if os.path.exists('_log.txt'):
-            os.replace('_log.txt', '_old_log.txt')
+        logging.addLevelName(60, 'SYSTEM')
+        logging.addLevelName(70, 'INIT')
+        logging.addLevelName(80, 'DIAGNOST')
 
-        logging.basicConfig(filename='_log.txt', filemode='w', format=MESSAGE_FORMAT)
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
+        file_filter = None
+        console_filter = None
+        if file_level is None:
+            file_filter = set_level_filter(HANDLER_LEVELS['CUSTOM_FILE_LEVELS'])
 
-        # для вывода логов в консоль расскоментировать строки ниже
-        #
-        # self.ch = logging.StreamHandler()
-        # self.ch.setLevel(logging.DEBUG)
-        # self.formatter = logging.Formatter(MESSAGE_FORMAT)
-        # self.ch.setFormatter(self.formatter)
-        # self.logger.addHandler(self.ch)
+        if console_level is None:
+            console_filter = set_level_filter(HANDLER_LEVELS['CUSTOM_CONSOLE_LEVELS'])
 
-    def isEnabledFor(self, level_index):
-        """
-        Функция для проверки наличия переданного уровня логирования в словаре существующих.
-        :param level_index: индекс уровня логирования.
-        :return:
-        True, если переданный индекс существует в словаре;
-        False, если отсутствует.
-        """
-        if level_index in LOGGING_LEVELS.keys():
-            return True
+        self._logger = logging.getLogger(name)
+        self._logger.setLevel(logging.DEBUG)
+
+        # добавление RotatingFileHandler для записи логов в файл
+        path = 'logs/logfile.log'
+        file_handler = RotatingFileHandler(path, backupCount=1)
+        if file_filter:
+            file_handler.addFilter(file_filter)
         else:
-            return False
+            file_handler.setLevel(file_level)
 
-    def make_log(self, level_index=0, msg='', is_error=False):
+        formatter = logging.Formatter(MESSAGE_FORMAT)
+        file_handler.setFormatter(formatter)
+        if len(logging.root.manager.loggerDict) == 1:
+            file_handler.doRollover()
+
+        self._logger.addHandler(file_handler)
+
+        # добавление StreamHandler для записи логов в консоль
+        console_handler = logging.StreamHandler(stream=sys.stdout)
+        if console_filter:
+            console_handler.addFilter(console_filter)
+        else:
+            console_handler.setLevel(console_level)
+
+        console_handler.setFormatter(formatter)
+        self._logger.addHandler(console_handler)
+
+    def make_log(self, level_index=10, msg='', is_error=False, *args):
         """
         Функция для записи сообщения с указанием уровня логирования и информацией о наличии ошибки.
         :param level_index: индекс уровеня логирования
@@ -59,12 +152,11 @@ class Logger(logging.getLoggerClass()):
         :param is_error: наличие ошибки
         """
         level_name = '[NOTSET]'
-        if self.isEnabledFor(level_index):
+        if isEnabledFor(level_index):
             level_name = f'[{LOGGING_LEVELS[level_index]}]'
 
-        error = '[ERROR]' if is_error else ''
-        msg = f'{error}{level_name} - {msg}'
-        self.logger.log(10, msg)
+        msg = make_msg(msg, is_error, level_index, level_name)
+        self._logger.log(level_index, msg, *args)
 
     def loger(self, level_index=0, msg='', is_error=False):
         """
@@ -77,3 +169,89 @@ class Logger(logging.getLoggerClass()):
         log_thread = threading.Thread(target=self.make_log, args=(level_index, msg, is_error))
         log_thread.start()
 
+    def system(self, msg, is_error=False, *args):
+        """
+        Фуцнкция для регистрации лога с уровнем SYSTEM.
+        :param msg: сообщение
+        :param is_error: наличие ошибки
+        :param args: дополнительные параметры
+        """
+        msg = make_msg(msg, is_error, 60)
+        log_thread = threading.Thread(target=self._logger.log, args=(60, msg, *args))
+        log_thread.start()
+
+    def init(self, msg, is_error=False, *args):
+        """
+        Фуцнкция для регистрации лога с уровнем INIT.
+        :param msg: сообщение
+        :param is_error: наличие ошибки
+        :param args: дополнительные параметры
+        """
+        msg = make_msg(msg, is_error, 70)
+        log_thread = threading.Thread(target=self._logger.log, args=(70, msg, *args))
+        log_thread.start()
+
+    def diagnost(self, msg, is_error=False, *args):
+        """
+        Фуцнкция для регистрации лога с уровнем DIAGNOST.
+        :param msg: сообщение
+        :param is_error: наличие ошибки
+        :param args: дополнительные параметры
+        """
+        msg = make_msg(msg, is_error, 80)
+        log_thread = threading.Thread(target=self._logger.log, args=(80, msg, *args))
+        log_thread.start()
+
+    def info(self, msg, is_error=False, *args):
+        """
+        Фуцнкция для регистрации лога с уровнем INFO.
+        :param msg: сообщение
+        :param is_error: наличие ошибки
+        :param args: дополнительные параметры
+        """
+        msg = make_msg(msg, is_error, 20)
+        log_thread = threading.Thread(target=self._logger.log, args=(20, msg, *args))
+        log_thread.start()
+
+    def debug(self, msg, is_error=False, *args):
+        """
+        Фуцнкция для регистрации лога с уровнем DEBUG.
+        :param msg: сообщение
+        :param is_error: наличие ошибки
+        :param args: дополнительные параметры
+        """
+        msg = make_msg(msg, is_error, 10)
+        log_thread = threading.Thread(target=self._logger.log, args=(10, msg, *args))
+        log_thread.start()
+
+    def warning(self, msg, is_error=False, *args):
+        """
+        Фуцнкция для регистрации лога с уровнем WARNING.
+        :param msg: сообщение
+        :param is_error: наличие ошибки
+        :param args: дополнительные параметры
+        """
+        msg = make_msg(msg, is_error, 30)
+        log_thread = threading.Thread(target=self._logger.log, args=(30, msg, *args))
+        log_thread.start()
+
+    def error(self, msg, *args):
+        """
+        Фуцнкция для регистрации лога с уровнем ERROR.
+        :param msg: сообщение
+        :param args: дополнительные параметры
+        """
+        msg = make_msg(msg, False, 40)
+        log_thread = threading.Thread(target=self._logger.log, args=(40, msg, *args))
+        log_thread.start()
+
+    def critical(self, msg, is_error=True, *args):
+        """
+        Фуцнкция для регистрации лога с уровнем CRITICAL.
+        :param msg: сообщение
+        :param is_error: наличие ошибки
+        :param args: дополнительные параметры
+        """
+        msg = make_msg(msg, is_error, 50)
+        log_thread = threading.Thread(target=self._logger.log, args=(50, msg, *args))
+        log_thread.start()
--- main.py
+++ main.py
@@ -1,14 +1,55 @@
+import logging
+import threading
+import time
+
 from logger import Logger
 
 
 def logger_test():
     log = Logger()
-    log.loger(0, 'test SYSTEM')
-    log.loger(3, 'test INFO and ERROR', True)
-    log.loger(2, 'test DIAGNOST')
-    log.loger(1, 'test INIT')
+    log.loger(60, 'test system log')
+    log.loger(70, 'test init log')
+    log.loger(80, 'test diagnost log')
+    log.loger(10, 'test debug log')
+    log.loger(20, 'test info log')
+    log.loger(30, 'test warning log')
+    log.loger(40, 'test error log')
+    log.loger(50, 'test critical log')
+
+    log.system('test system log')
+    log.init('test init log')
+    log.diagnost('test diagnost log')
+    log.info('test info log')
+    log.debug('test debug log')
+    log.warning('test warning log')
+    log.error('test error log')
+    log.critical('test critical log')
+
+    log.loger(60, 'test error system log', True)
+    log.loger(70, 'test error init log', True)
+    log.loger(80, 'test error diagnost log', True)
+    log.loger(10, 'test error debug log', True)
+    log.loger(20, 'test error info log', True)
+    log.loger(30, 'test error warning log', True)
+    log.loger(40, 'test error warning log')
+    log.loger(50, 'test error warning log', True)
+
+    log.system('test error system log', True)
+    log.init('test error init log', True)
+    log.diagnost('test error diagnost log', True)
+    log.info('test error info log', True)
+    log.debug('test error debug log', True)
+    log.warning('test error warning log', True)
+    log.error('test error error log')
+    log.critical('test error critical log')
+
+
+def logger_name_test():
+    log = Logger('logger_name_test')
+    log.info('test loger name')
 
 
 if __name__ == '__main__':
     logger_test()
+    logger_name_test()
 
